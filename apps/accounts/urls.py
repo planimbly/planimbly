12c3897ee93f8cfdc6522aaae5ffae4d349e87ee
pyron <pyron@wmi.amu.edu.pl>
2022-04-11 21:51:00 +0200

    Login backend

---
+++ apps/accounts/urls.py
@@ -0,0 +1,37 @@
+"""planimbly URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from .views import *
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='accounts/password_change.html'),
+         name='password_change'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='accounts/password_reset.html'),
+         name='password_reset'),
+    path('password_reset_done/',
+         auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'),
+         name='password_reset_complete'),
+    path('password_activate/<uidb64>/<token>/', PasswordActivateView.as_view(), name='password_activate')
+]
