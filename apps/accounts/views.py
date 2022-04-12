5a5802753f3006a509fd295c5b33ee0ff1ac1b79
pyron <pyron@wmi.amu.edu.pl>
2022-04-12 14:02:15 +0200

    Rest framework, setting up API

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -2,3 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views import View
+from rest_framework import viewsets, permissions
+
+from apps.accounts.models import Employee
+from apps.accounts.serializers import EmployeeSerializer
+
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+    permission_classes = [permissions.IsAuthenticated]
