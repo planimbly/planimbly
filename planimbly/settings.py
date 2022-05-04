cdcc24bf357ca356a783dc0e14b6edb3f0a62529
pyron <pyron@wmi.amu.edu.pl>
2022-05-04 21:48:51 +0200

    Employees to units and workplaces

--- planimbly/settings.py
+++ planimbly/settings.py
@@ -136,6 +136,7 @@ AUTHENTICATION_BACKENDS = (
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
