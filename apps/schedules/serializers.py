dabe266f2675ab3e1c47fa062e573e8286a558e9
pyron <pyron@wmi.amu.edu.pl>
2022-05-22 12:09:24 +0200

    get schedule api view

--- apps/schedules/serializers.py
+++ apps/schedules/serializers.py
@@ -6,7 +6,7 @@ from .models import ShiftType
 class ShiftTypeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ShiftType
-        fields = ['id', 'hour_start', 'hour_end', 'name', 'active_days', 'is_algorithm']
+        fields = ['id', 'hour_start', 'hour_end', 'name', 'active_days', 'is_used']
         extra_kwargs = {
             'id': {'read_only': True}
         }
