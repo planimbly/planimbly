07b46e00804503dab1cba7aa8fd61276a0608ccf
SaluSL <szysla@st.amu.edu.pl>
2022-05-25 14:44:15 +0200

    calendar code cleanup PNBY-85

--- apps/schedules/views.py
+++ apps/schedules/views.py
@@ -24,7 +24,7 @@ class ScheduleManageView(TemplateView):
         context = super().get_context_data(**kwargs)
         workplace_list = Workplace.objects.filter(workplace_unit_id=self.kwargs['unit_pk'])
         context['workplace_list'] = workplace_list
-        # SaluSL: added more context to schedule_manage
+
         select_workplace = dict()
         for obj in workplace_list:
             select_workplace.setdefault(obj.workplace_unit_id, []).append({"id": obj.id, "name": obj.name})
