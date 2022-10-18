69f1e269c0c7dc1204121477f8c7b0093636e3bd
pyron <pyron@wmi.amu.edu.pl>
2022-10-18 13:34:45 +0200

    Not finished absence crude. Commit dla Michała

--- apps/schedules/urls.py
+++ apps/schedules/urls.py
@@ -2,21 +2,23 @@ from django.urls import path, include
 from rest_framework import routers
 
 from apps.schedules.views import ShiftTypeManageView, ShiftTypeViewSet, ScheduleManageView, ScheduleGetApiView, \
-    ScheduleCreateApiView, ShiftManageApiView, PreferenceViewSet
+    ScheduleCreateApiView, ShiftManageApiView, PreferenceViewSet, AbsenceViewSet, AbsenceManageView
 
+shiftType_router = routers.DefaultRouter()
+shiftType_router.register(r'shiftType', ShiftTypeViewSet, basename='shiftType')
 router = routers.DefaultRouter()
-router.register(r'shiftType', ShiftTypeViewSet, basename='shiftType')
-preference_router = routers.DefaultRouter()
-preference_router.register(r'preference', PreferenceViewSet, basename='preference')
+router.register(r'preference', PreferenceViewSet, basename='preference')
+router.register(r'absence', AbsenceViewSet, basename='absence')
 
 urlpatterns = [
     path('shiftType_manage/', ShiftTypeManageView.as_view(), name="shiftType_manage"),
     path('schedule_manage/', ScheduleManageView.as_view(), name="schedule_manage"),
+    path('absence_manage/<int:employee_pk>', AbsenceManageView.as_view(), name="absence_manage"),
 
     # API urls
     path('api/schedule_create/', ScheduleCreateApiView.as_view(), name='schedule_create'),
-    path('api/<int:workplace_pk>/', include(router.urls)),
-    path('api/', include(preference_router.urls)),
+    path('api/<int:workplace_pk>/', include(shiftType_router.urls)),
+    path('api/', include(router.urls)),
     path('api/shift_manage/', ShiftManageApiView.as_view(), name='shift_manage'),
     path('api/<int:workplace_pk>/schedule_get/', ScheduleGetApiView.as_view(),
          name='schedule_get')
