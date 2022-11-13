303807f9908c5fcc3c80bd55c2083ef52eec7281
SaluSL <szysla@st.amu.edu.pl>
2022-11-13 16:38:51 +0100

    small pdf adjustments

--- static/js/components/ScheduleReport.js
+++ static/js/components/ScheduleReport.js
@@ -24,29 +24,83 @@ export default {
         <table class="table table-success table-striped">
           <thead>
             <tr>
-              <th scope="col">#</th>
-              <th scope="col">First</th>
-              <th scope="col">Last</th>
-              <th scope="col">Handle</th>
+              <th scope="col">Zmiana</th>
+              <th scope="col">1</th>
+              <th scope="col">2</th>
+              <th scope="col">3</th>
+              <th scope="col">4</th>
+              <th scope="col">5</th>
+              <th scope="col">6</th>
+              <th scope="col">7</th>
+              <th scope="col">8</th>
+              <th scope="col">9</th>
+              <th scope="col">10</th>
+              <th scope="col">11</th>
+              <th scope="col">12</th>
+              <th scope="col">13</th>
+              <th scope="col">14</th>
+              <th scope="col">15</th>
+              <th scope="col">16</th>
+              <th scope="col">17</th>
+              <th scope="col">18</th>
+              <th scope="col">19</th>
+              <th scope="col">20</th>
+              <th scope="col">21</th>
+              <th scope="col">22</th>
+              <th scope="col">23</th>
+              <th scope="col">24</th>
+              <th scope="col">25</th>
+              <th scope="col">26</th>
+              <th scope="col">27</th>
+              <th scope="col">28</th>
+              <th scope="col">29</th>
+              <th scope="col">30</th>
+              <th scope="col">31</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
+              <th scope="row">M</th>
+              <td>A</td>
+              <td>D</td>
+              <td>4</td>
+              <td>6</td>
+              <td>8</td>
+              <td>8</td>
+              <td>4</td>
+              <td>3</td>
+              <td>2</td>
+              <td>1</td>
+              <td>1</td>
+              <td>4</td>
+              <td>2</td>
+              <td>5</td>
+              <td>6</td>
+              <td>7</td>
+              <td>8</td>
+              <td>9</td>
+              <td>11</td>
+              <td>14</td>
+              <td>15</td>
+              <td>16</td>
+              <td>1</td>
+              <td>3</td>
+              <td>54</td>
+              <td>5</td>
+              <td>1</td>
+              <td>2</td>
+              <td>5</td>
+              <td>1</td>
+              <td>2</td>
+
             </tr>
             <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
+              <th scope="row">A</th>
+              
             </tr>
             <tr>
-              <th scope="row">3</th>
-              <td colspan="2">Larry the Bird</td>
-              <td>@twitter</td>
+              <th scope="row">N</th>
+              
             </tr>
           </tbody>
         </table>
