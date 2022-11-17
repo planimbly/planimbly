3d176b9a4acdf63988a71a48f7631abe81e19e50
Miron Pawlak <mip@impicode.com>
2022-11-17 23:15:20 +0100

    admin add

--- apps/organizations/admin.py
+++ apps/organizations/admin.py
@@ -1,1 +1,8 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Organization, Unit, Workplace, WorkplaceClosing
+
+admin.site.register(Organization)
+admin.site.register(Unit)
+admin.site.register(Workplace)
+admin.site.register(WorkplaceClosing)
--- apps/schedules/admin.py
+++ apps/schedules/admin.py
@@ -1,1 +1,16 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Schedule, ShiftType, Preference, Assignment, Shift, Absence
+
+admin.site.register(Schedule)
+admin.site.register(ShiftType)
+admin.site.register(Preference)
+
+
+class ShiftAdmin(admin.ModelAdmin):
+    list_filter = ('employee', 'schedule')
+
+
+admin.site.register(Shift, ShiftAdmin)
+admin.site.register(Absence)
+admin.site.register(Assignment)
