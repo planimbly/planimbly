2d8ae54ad60d4dd19fc516847f136ee13c949401
meehawek <myf.goralczyk@gmail.com>
2022-12-02 11:48:05 +0100

    deployment 8 cores 5 ghz 2 gb ram less in 1 sec

--- apps/accounts/apps.py
+++ apps/accounts/apps.py
@@ -1,10 +1,10 @@
 from django.apps import AppConfig
-from django.core.management import call_command
+# from django.core.management import call_command
 
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.accounts'
 
-    def ready(self):
-        call_command('create_groups')
+    # def ready(self):
+    #     call_command('create_groups')
--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.base_user import BaseUserManager
-from django.contrib.auth.models import AbstractUser, Group
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 from apps.organizations.models import Organization, Workplace, Unit
