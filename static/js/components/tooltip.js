e0b12f72636bad9abd7cddabb9b3d5b2f0b8b197
meehawek <myf.goralczyk@gmail.com>
2023-01-09 15:11:39 +0100

    tooltip changes, date constraints, frontend fixes

--- static/js/components/tooltip.js
+++ static/js/components/tooltip.js
@@ -4,17 +4,36 @@ export default {
     text: {
       type: String,
       required: true
+    },
+    elementid: {
+      type: String,
+      required: true
+    },
+  },
+  data(){
+    return {
+      cWidth: null,
+    }
+  },
+  methods: {
+
+  },
+  computed: {
+    updateId(){
+      if ( document.getElementById(this.elementid) != null) {
+        this.cWidth = document.getElementById(this.elementid).clientWidth
+        console.log(this.cWidth)
+      }
     }
   },
   template: `
   <div class="PN-tooltip-container">
+    [[ updateId ]]
     <slot />
-    <div
-      class="PN-tooltip"
-    >
-      <span
-        class="PN-text"
-      >[[ text ]]</span>
+    <div class="PN-tooltip">
+      <span class="PN-text">
+        [[ text ]]
+      </span>
     </div>
   </div>`
 }
